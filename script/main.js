c9414f22e5f6cc8cdb20ee7668e50faff7e15d9c
JacopoGrippiotti <jacopo.grippiotti@gmail.com>
2023-07-05 18:12:40 +0200

    added data and methods in main.js file to do get call to server.php

--- script/main.js
+++ script/main.js
@@ -3,15 +3,24 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-
+            apiUrl:'../server.php',
+            cdList:[]
         }
     },
 
     methods: {
-        
+        getcdList(){
+            axios.get(this.apiUrl,{
+                params:{}
+            })
+            .then((response) => {
+                console.log(response);
+                this.cdList = response.data;
+            })
+        }
     },
 
     created() {
-        
+      this.getcdList();
     },
 }).mount("#app")
